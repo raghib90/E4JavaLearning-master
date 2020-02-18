eb532d6c7ab67131a087d26c3e06a81588faf614
Raghib Anjum <raghib90@gmail.com>
2020-02-18 21:42:17 +0530

    Initial commit

---
+++ src/firstPack/Addition.java
@@ -0,0 +1,37 @@
+package firstPack;
+
+public class Addition {
+	
+	
+	public static void main(String[] args) {
+		
+		int fno, sno, res, fn;
+		
+		fno=3;
+		sno = 5;
+		fn=6;
+		
+		res = fno + sno;
+		int mul = fno*sno;
+		
+		float fVar = 3.40f;
+		double dvar = 3.456;
+		
+		boolean flag = true;
+		
+		long lvar = 2213323;
+		
+		System.out.println("The value of float var is "+fVar);
+		System.out.println("The value of double var is "+dvar);
+		System.out.println("The value of boolean var is "+flag);
+		System.out.println("The value of long var is "+lvar);
+		System.out.println("The product of two numbers "+mul);
+		
+		System.out.println("The sum of two numbers is "+res);
+		
+		
+		
+		
+	}
+
+}
---
+++ src/firstPack/HelloWorld.java
@@ -0,0 +1,16 @@
+package firstPack;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("Name");
+				System.out.println("Anjum");
+		System.out.println("Alam");
+
+	}
+	
+	
+
+}
